8e4ddaa94d5d800eb693ccfe38b1ceda19705278
Nkem Ohanenye <tuj20157@temple.edu>
2020-04-26 21:45:44 -0400

    the hand can be detected and identified

--- app/src/main/java/com/example/rockpaperscissor/DisplayCameraLayout.java
+++ app/src/main/java/com/example/rockpaperscissor/DisplayCameraLayout.java
@@ -2,34 +2,29 @@ package com.example.rockpaperscissor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.graphics.Camera;
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfInt;
 import org.opencv.core.MatOfPoint;
-import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.videoio.VideoCapture;
 
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 
@@ -38,11 +33,14 @@ public class DisplayCameraLayout extends AppCompatActivity implements CameraBrid
     private static String TAG = "DisplayCameraLayout";
     JavaCameraView javaCameraView;
 
+    List<String> hand = new ArrayList<>();
+    private static String choice = "";
+
     // enables the camera to keep running
     BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(DisplayCameraLayout.this) {
         @Override
         public void onManagerConnected(int status) {
-            switch (status){
+            switch (status) {
                 case BaseLoaderCallback.SUCCESS:
                     javaCameraView.enableView();
                     break;
@@ -74,37 +72,106 @@ public class DisplayCameraLayout extends AppCompatActivity implements CameraBrid
     public void onCameraViewStopped() {
     }
 
-     @Override
+    @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-         Mat mRgba = inputFrame.rgba();
-         List<MatOfPoint> contours = new ArrayList<>();
-         Mat gray = new Mat();
-         Mat hierarchy = new Mat();
-         // blurs the image to try and get the focal point
-         Imgproc.GaussianBlur(mRgba, gray, new Size(7, 7), 2,  2);
-         // uses Canny's matricies to find the contour lines of an image
-         Imgproc.Canny(mRgba, gray, 120, 200);
-         // finds the matricies
-         Imgproc.findContours(gray, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0,0));
+        Mat mRgba = inputFrame.rgba();
+        int max_area = 0;
+        int savedContour = 0;
 
-         //hierarchy.release();
+        // draws the contours
+        List<MatOfPoint> contours = new ArrayList<>();
+        Rect ROI = new Rect(0, 0, mRgba.cols(), mRgba.rows());
+        //Mat gray = new Mat(mRgba, ROI);
+        Mat gray = new Mat(mRgba, ROI);
+        Mat cannyOutput = new Mat();
+        Mat hierarchy = new Mat();
+        Imgproc.cvtColor(mRgba, gray, Imgproc.COLOR_RGB2GRAY);
+        // blurs the image to try and get the focal point
+        Imgproc.GaussianBlur(gray, gray, new Size(7, 7), 2, 2);
+        // uses Canny's matricies to find the contour lines of an image
+        Imgproc.Canny(gray, cannyOutput, 150, 200);
+        // finds the matricies
+        Imgproc.findContours(cannyOutput, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
 
-         for (int contourIdx = 0; contourIdx < contours.size(); contourIdx++){
-             Imgproc.drawContours(mRgba, contours, contourIdx, new Scalar(250,0,0), 3);
-         }
+        List<MatOfPoint> hullList = new ArrayList<>();
+        for (MatOfPoint contour : contours) {
+            MatOfInt hull = new MatOfInt();
+            Imgproc.convexHull(contour, hull, false);
+            Point[] contourArray = contour.toArray();
+            Point[] hullPoints = new Point[hull.rows()];
+            List<Integer> hullContourIdxList = hull.toList();
+            for (int i = 0; i < hullContourIdxList.size(); i++) {
+                hullPoints[i] = contourArray[hullContourIdxList.get(i)];
+            }
+            hullList.add(new MatOfPoint(hullPoints));
+        }
 
-         return mRgba;
+        for (int i = 0; i < contours.size(); i++) {
+            double area = Imgproc.contourArea(contours.get(i));
+            if (area > max_area) {
+                max_area = (int) area;
+                savedContour = i;
+            }
+        }
+        Imgproc.drawContours(mRgba, contours, savedContour, new Scalar(255, 0, 0), 3);
+        Imgproc.drawContours(mRgba, hullList, savedContour, new Scalar(0, 0, 255), 3);
+
+        // flips the video
+        Core.flip(mRgba.t(), cannyOutput, 1);
+        Imgproc.resize(cannyOutput, cannyOutput, mRgba.size());
+
+        // write the directions on the screen
+        Imgproc.putText(cannyOutput, getText(R.string.make_hand_gesture).toString(), new Point(10, 50), Core.FONT_HERSHEY_PLAIN, 2.5, new Scalar(255, 255, 255), 3);
+
+
+        // output finger result, when 50 values in list the last value is chosen as the answer
+        if (contours.size() == 5) {
+            Imgproc.putText(cannyOutput, "Paper", new Point(30, 100), Core.FONT_HERSHEY_PLAIN, 4, new Scalar(255, 255, 255), 3);
+            hand.add("Paper");
+            /*if (hand.size() >= 50) {
+                choice = hand.get(hand.size() - 1);
+                hand.clear();
+                Intent intent = new Intent(this, DisplayCameraResults.class);
+                intent.putExtra("playerChoice", choice);
+                startActivity(intent);
+            }*/
+        } else if (contours.size() > 8) {
+            Imgproc.putText(cannyOutput, "Scissors", new Point(30, 100), Core.FONT_HERSHEY_PLAIN, 4, new Scalar(255, 255, 255), 3);
+            hand.add("Scissors");
+            /*if (hand.size() >= 50) {
+                choice = hand.get(hand.size() - 1);
+                hand.clear();
+                Intent intent = new Intent(this, DisplayCameraResults.class);
+                intent.putExtra("playerChoice", choice);
+
+                startActivity(intent);
+            }*/
+        } else if (contours.size() > 0) {
+            Imgproc.putText(cannyOutput, "Rock", new Point(30, 100), Core.FONT_HERSHEY_PLAIN, 4, new Scalar(255, 255, 255), 3);
+            hand.add("Rock");
+            /*if (hand.size() >= 50) {
+                choice = hand.get(hand.size() - 1);
+                hand.clear();
+                Intent intent = new Intent(this, DisplayCameraResults.class);
+                intent.putExtra("playerChoice", choice);
+                startActivity(intent);
+            }*/
+        }
+
+        return cannyOutput;
     }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
     }
 
-    /** Handles the cases in which the camera is closed */
+    /**
+     * Handles the cases in which the camera is closed
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (javaCameraView != null){
+        if (javaCameraView != null) {
             javaCameraView.disableView();
         }
     }
@@ -112,21 +179,23 @@ public class DisplayCameraLayout extends AppCompatActivity implements CameraBrid
     @Override
     protected void onPause() {
         super.onPause();
-        if (javaCameraView != null){
+        if (javaCameraView != null) {
             javaCameraView.disableView();
         }
     }
 
-    /** checks to see if the debugger is running, if there is a crash, the camera closes */
+    /**
+     * checks to see if the debugger is running, if there is a crash, the camera closes
+     */
     @Override
     protected void onResume() {
         super.onResume();
-        if (OpenCVLoader.initDebug()){
+        if (OpenCVLoader.initDebug()) {
             Log.d(TAG, "OpenCV is Configured or Connected Successfully.");
             baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
         } else {
-          Log.d(TAG, "OpenCV not working or loaded.");
-          OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, baseLoaderCallback);
+            Log.d(TAG, "OpenCV not working or loaded.");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, baseLoaderCallback);
         }
     }
 }
---
+++ app/src/main/java/com/example/rockpaperscissor/DisplayCameraResults.java
@@ -0,0 +1,82 @@
+package com.example.rockpaperscissor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class DisplayCameraResults extends AppCompatActivity {
+
+    String options[] ={"Rock","Paper","Scissor"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_button_results);
+
+        Intent intent = getIntent();
+        Bundle bundle = getIntent().getExtras();
+
+        // all reference variables that need to be created
+        assert bundle != null;
+        String choice = bundle.getString("playerChoice");
+        TextView result = findViewById(R.id.lblResults);
+        TextView playerChoice = findViewById(R.id.lblPlayerChoice);
+        TextView comChoice = findViewById(R.id.lblComChoice);
+        Button btnResult = findViewById(R.id.btnResult);
+
+        playerChoice.setText(choice);
+        comChoice.setText(checkComputerChoice());
+
+        result.setText(displayResult(playerChoice, comChoice, btnResult));
+    }
+    // checks what the computer picks
+    protected String checkComputerChoice(){
+        Random r= new Random();
+        String choice = options[r.nextInt(2)];
+        if(choice.equals(options[0])){
+            return choice; // computer chooses rock
+        }
+        else if (choice.equals(options[1])){
+            return choice; // computer chooses paper
+        }
+        else if (choice.equals(options[2])){
+            return choice; // computer chooses scissors
+        } else {
+            return "";
+        }
+    }
+    // checks which one wins
+    protected String displayResult(TextView player, TextView com, Button result) {
+        if (player.getText().toString().equals(options[0]) && com.getText().toString().equals(options[2]) ||
+                player.getText().toString().equals(options[1]) && com.getText().toString().equals(options[0]) ||
+                player.getText().toString().equals(options[2]) && com.getText().toString().equals(options[1])) {
+            result.setText(getString(R.string.play_again));
+            return getString(R.string.player_win);  // player wins
+        } else if (player.getText().toString().equals(options[0]) && com.getText().toString().equals(options[1]) ||
+                player.getText().toString().equals(options[1]) && com.getText().toString().equals(options[2]) ||
+                player.getText().toString().equals(options[2]) && com.getText().toString().equals(options[0])){
+            result.setText(getString(R.string.play_again));
+            return getString(R.string.com_win);  // computer wins
+        } else {
+            result.setText(getString(R.string.pick_another));
+            return getString(R.string.tie_win);  // tie
+        }
+    }
+    // checks to see what value the result button has
+    public void btnResultPress(View view) {
+        Button btnResult = findViewById(R.id.btnResult);
+        if (btnResult.getText().toString().equals(getString(R.string.play_again))) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, DisplayCameraLayout.class);
+            startActivity(intent);
+        }
+    }
+}
